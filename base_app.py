3da70a43503cf5047a61e70187d0e77e79c07a81
Chesley Rogerson <ckrogerson@gmail.com>
2023-06-18 22:45:52 +0200

    Additional pages added and relevant training data

--- base_app.py
+++ base_app.py
@@ -33,7 +33,8 @@ news_vectorizer = open("resources/tfidfvect.pkl","rb")
 tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
 
 # Load your raw data
-raw = pd.read_csv("resources/train.csv")
+#raw = pd.read_csv("resources/train.csv")
+raw = pd.read_csv("resources/Training_Data.csv")
 
 # The main function where we will build the actual app
 def main():
@@ -41,26 +42,33 @@ def main():
 
 	# Creates a main title and subheader on your page -
 	# these are static across all pages
-	st.title("Tweet Classifer")
-	st.subheader("Climate change tweet classification")
+	#st.title("Tweet Classifier")
+
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
+	options = ["Overview and Instructions", "Make a Prediction", "Explore the Data", "Contributors"]
 	selection = st.sidebar.selectbox("Choose Option", options)
 
-	# Building out the "Information" page
-	if selection == "Information":
-		st.info("General Information")
-		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
-
-		st.subheader("Raw Twitter data and label")
-		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
-			st.write(raw[['sentiment', 'message']]) # will write the df to the page
+	if selection == "Overview and Instructions":
+		
+		st.info("# Tweet Classifier")
+		st.subheader("Climate change tweet classification")
+		st.markdown("#")
+		
+		st.info("Overview")
+		st.markdown("Description of scope and utility of application")
+		st.markdown("#")
+		st.info("Instructions", icon="ℹ️")
+		st.markdown("Provide instructions for use of features")
 
 	# Building out the predication page
-	if selection == "Prediction":
+	if selection == "Make a Prediction":
+		
+		st.info("# Tweet Classifier")
+		#st.subheader("Climate change tweet classification")
+		st.markdown("#")
+		
 		st.info("Prediction with ML Models")
 		# Creating a text box for user input
 		tweet_text = st.text_area("Enter Text","Type Here")
@@ -77,6 +85,35 @@ def main():
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
 			st.success("Text Categorized as: {}".format(prediction))
+
+	# Building out the "Information" page
+	if selection == "Explore the Data":
+		
+		st.info("# Tweet Classifier")
+		#st.subheader("Climate change tweet classification")
+		st.markdown("#")
+		
+		st.info("General Information")
+		# You can read a markdown file from supporting resources folder
+		st.markdown("Some information here")
+
+		st.subheader("Raw Twitter data and label")
+		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
+			st.write(raw[['sentiment', 'message']]) # will write the df to the page
+
+	if selection == "Contributors":
+		
+		st.info("# Tweet Classifier")
+		#st.subheader("Climate change tweet classification")
+		st.markdown("#")
+		
+		st.info("Team")
+
+		team_list = ["Sias Willemse", "Chesley Rogerson", "Thato Molapisi", "Thabiso Khoza", "Thebe Dikobo", "Boitumelo Mphahlele"]
+		
+		for member in team_list:
+
+			st.markdown(member)
 
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
